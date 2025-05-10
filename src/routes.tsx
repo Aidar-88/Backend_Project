db27fdc2d4ada65e427dbe89edefdfdd8f2a5b73
Aidar <aidar.omir7iha@gmail.com>
2025-05-11 03:39:48 +0500

    update routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage";
 import CatalogPage from "./pages/CatalogPage";
 import FavoritesPage from "./pages/FavoritesPage";
 import CartPage from "./pages/CartPage";
-import { useAuth } from "./context/AuthContext";
+// import { useAuth } from "./context/AuthContext";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import { ReactElement } from "react";
 
@@ -14,11 +14,11 @@ interface PrivateRouteProps {
 }
 
 // Временно отключаем проверку авторизации
-const PrivateRoute = ({ children }: PrivateRouteProps) => {
-    // const { token } = useAuth();
-    // return token ? children : <Navigate to="/login" replace />;
-    return children;
-};
+// const PrivateRoute = ({ children }: PrivateRouteProps) => {
+//     //  const { token } = useAuth();
+//     // return token ? children : <Navigate to="/login" replace />;
+//     return children;
+// };
 
 export const AppRoutes = () => {
     return (
